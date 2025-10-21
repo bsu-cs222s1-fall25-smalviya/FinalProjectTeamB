04c7eee2b0602770c61d600fd05307d69300e32d
Ca5ity <cassandra.lewis4503@gmail.con>
2025-10-20 21:10:51 -0400

    Added Caluclate class

---
+++ src/main/java/edu/bsu/cs222/CalculateProbabilities.java
@@ -0,0 +1,8 @@
+package edu.bsu.cs222;
+
+public class CalculateProbabilities {
+
+    /* method to calculate probability formula based on information given
+
+     */
+}
--- src/test/java/edu/bsu/cs222/TestFormatOutput.java
+++ src/test/java/edu/bsu/cs222/TestFormatOutput.java
@@ -1,4 +1,11 @@
 package edu.bsu.cs222;
 
+import org.junit.jupiter.api.Test;
+
 public class TestFormatOutput {
+
+    @Test
+    public void testReformatcalc() {
+        // assert
+    }
 }
