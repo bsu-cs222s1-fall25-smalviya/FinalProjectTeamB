006165d8f89300f778cc962864f2155b6c04c2ad
Ca5ity <cassandra.lewis4503@gmail.con>
2025-10-23 09:49:28 -0400

    added genshin probability

--- src/main/java/edu/bsu/cs222/CalculateProbabilities.java
+++ src/main/java/edu/bsu/cs222/CalculateProbabilities.java
@@ -3,9 +3,19 @@ package edu.bsu.cs222;
 public class CalculateProbabilities {
 
 
-    public double complementaryProbability(double Probability, int numOfPulls) {
+    public double complementaryProbability(double probability, int numOfPulls) {
 
-        return Math.pow(1 - (1- Probability), numOfPulls);
+        return Math.pow(1 - (1- probability), numOfPulls);
+    }
+
+    public double genshinProbability (double softprobability, double hardProbability, int numOfPulls){
+        double finalProbability=0;
+        for (int i = 0; i <= numOfPulls ; i++) {
+            if (i > 75){
+                finalProbability= hardProbability;
+            } else  finalProbability =  Math.pow(1 - (1 - softprobability), numOfPulls);
+        }
+        return finalProbability;
     }
 
 }
