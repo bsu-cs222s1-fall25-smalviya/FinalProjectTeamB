c2b806315f9d0b42ec2f1a29f25f9e33f4b203e3
A-Reilly <ashreilly135@gmail.com>
2025-10-27 19:31:50 -0400

    Made tests to check the input

--- src/test/java/edu/bsu/cs222/InputTest.java
+++ src/test/java/edu/bsu/cs222/InputTest.java
@@ -1,4 +1,23 @@
 package edu.bsu.cs222;
 
+import org.junit.jupiter.api.Test;
+import java.util.Scanner;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class InputTest {
+@Test
+    public void testGameInput() {
+    Scanner tester = new Scanner("1");
+    Input game = new Input(tester);
+
+    int gameInput = game.getUserInput();
+    assertEquals(2, gameInput);
 }
+@Test
+    public void testRarityInput(){
+    Scanner tester = new Scanner("6");
+    Input rarity = new Input(tester);
+
+    int rarityInput = rarity.getUserRarity();
+    assertEquals(6, rarityInput);
+}}
