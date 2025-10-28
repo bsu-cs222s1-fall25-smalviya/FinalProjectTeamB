dec5458830fc44afa64856a9b36ddcbab19fd16f
Ca5ity <cassandra.lewis4503@gmail.con>
2025-10-27 21:29:38 -0400

    fixed spelling for retrieve and gacha

---
+++ src/test/java/edu/bsu/cs222/TestRetrieveDataFromDatabase.java
@@ -0,0 +1,24 @@
+package edu.bsu.cs222;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestRetrieveDataFromDatabase {
+
+    @Test
+    public void testGetPokemonTCGPocketCardProbability(){
+
+        RetrieveDataFromDatabase pull = new RetrieveDataFromDatabase();
+        double result = pull.gachaGameProbability("Pokemon TCG Pocket", "Single Booster Pack", 2);
+        Assertions.assertEquals(0.2378, result);
+    }
+
+    @Test
+    public void testGetPokemonTCGPocketCardProbability1(){
+
+        RetrieveDataFromDatabase pull = new RetrieveDataFromDatabase();
+        double result = pull.gachaGameProbability("Pokemon TCG Pocket", "Single Booster Pack",8);
+        Assertions.assertEquals(0.0166, result);
+    }
+
+}
