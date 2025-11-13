1bbe40dc783ca9f30883044291698237121dd8dc
tatumph <149801019+tatumph@users.noreply.github.com>
2025-11-12 19:49:38 -0500

    Fixed Main and worked on User Input.

--- src/main/java/edu/bsu/cs222/Input.java
+++ src/main/java/edu/bsu/cs222/Input.java
@@ -12,53 +12,78 @@ public class Input {
     public Input(){//This is the default input
         this.scanner = new Scanner(System.in);
     }
+
     public int getUserInput(){
 
         System.out.print("Please enter the Gacha Game (Enter 1 for Pokemon TCGP, 2 for Infinity Nikki, or 3 for Genshin Impact): ");
-        return Integer.parseInt(scanner.nextLine());
+        return Integer.parseInt(scanner.next());
     }
+
     public int getUserRarity(String gameTitle) {
-        return switch (gameTitle) {
+        int rarity;
+        switch (gameTitle) {
             case "Pokemon TCGP" -> {
                 System.out.print("Please enter a number for the rarity (0 - Uncommon, 1 - Rare, 2 - Ultra Rare, " +
                         "3 - Illustration Rare, 4 - Special Illustration Rare, 5 - Immersive, 6 - Shiny rare, 7 - Double Shiny Rare, 8 - Crown Rare): ");
-                yield Integer.parseInt(scanner.nextLine());
+                rarity = Integer.parseInt(scanner.next());
+                break;
             }
             case "Infinity Nikki" -> {
-                System.out.print("Please enter the number for the rarity (0 - 3★, 1 - 4★, 2 - 5★): ");
-                yield Integer.parseInt(scanner.nextLine());
+                System.out.print("Please enter the number for the rarity (0 - 3-Star, 1 - 4-Star, 2 - 5-Star): ");
+                rarity = Integer.parseInt(scanner.next());
+                break;
             }
             case "Genshin Impact" -> {
-                System.out.print("Please enter the number for your desired rarity: (0 - 3★, 1 - 4★, 2 - 5★): ");
-                yield Integer.parseInt(scanner.nextLine());
+                System.out.print("Please enter the number for your desired rarity: (0 - 3-Star, 1 - 4-Star, 2 - 5-Star): ");
+                rarity = Integer.parseInt(scanner.next());
+                break;
             }
-            default -> Integer.parseInt(scanner.nextLine());
-        };
-
+            default -> rarity = Integer.parseInt(null);
+        }
+        return rarity;
     }
+
     public String getUserBanner(String gameTitle) {
         String rarity = null;
         switch (gameTitle){
             case "Pokemon TCGP":
                 System.out.print("Please enter a number for the banner (0 - Single Standard Booster Pack): ");
-                rarity = scanner.nextLine();
+                rarity = scanner.next();
+                rarity = "Single Standard Booster Pack";
                 break;
             case "Infinity Nikki":
-                System.out.print("Please enter a number for the banner (0 - Surprise-O-Matic, 1 - 5★ Resonance, 2 - 4★ Resonance): ");
-                rarity = scanner.nextLine();
+                System.out.print("Please enter a number for the banner (0 - Surprise-O-Matic, 1 - 5-Star Resonance, 2 - 4-Star Resonance): ");
+                rarity = scanner.next();
+                switch (rarity){
+                    case "0" ->
+                        rarity = "Surprise-O-Matic";
+                    case "1" ->
+                        rarity = "5-Star Resonance";
+                    case "2" ->
+                        rarity = "4-Star Resonance";
+                }
                 break;
             case "Genshin Impact":
-                System.out.print("Please enter a number for the banner (0 - Limited Character, 1 - 5★, 2 - 4★ )");
-                rarity = scanner.nextLine();
+                System.out.print("Please enter a number for the banner (0 - Limited Character, 1 - 5-Star, 2 - 4-Star )");
+                rarity = scanner.next();
+                switch (rarity){
+                    case "0" ->
+                        rarity = "Limited Character";
+                    case "1" ->
+                        rarity = "5-Star";
+                    case  "2" ->
+                        rarity = "4-Star";
+                }
                 break;
-
         }
         return rarity;
     }
+
     public int getUserNumOfPulls(){
         System.out.print("Please enter the number of pulls: ");
-        return Integer.parseInt(scanner.nextLine());
+        return Integer.parseInt(scanner.next());
     }
+
     public boolean genshinQuestion(String gameTitle){
 
         if (!gameTitle.equals("Genshin Impact")) {
--- src/main/java/edu/bsu/cs222/Main.java
+++ src/main/java/edu/bsu/cs222/Main.java
@@ -1,5 +1,7 @@
 package edu.bsu.cs222;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Input input = new Input();
@@ -7,10 +9,11 @@ public class Main {
         FormatOutput formatOutput = new FormatOutput();
         RetrieveDataFromDatabase retrieve = new RetrieveDataFromDatabase();
         CalculateProbabilities calculate = new CalculateProbabilities();
+        GachaPullSim pullSim = new GachaPullSim();
         How how = new How();
 
         int game = input.getUserInput();
-        String gameTitle = "";
+        String gameTitle = null;
 
         switch (game){
             case 1 ->
@@ -35,8 +38,17 @@ public class Main {
 
         int numOfPulls = input.getUserNumOfPulls();
 
-        double singlePull = retrieve.gachaGameProbability(gameTitle, bannerName, rarity);
-        double finalProbability = calculate.complementaryProbability(singlePull, numOfPulls);
+        BannerStats bannerStats = retrieve.gachaGameProbability(gameTitle, bannerName, rarity);
+        double singlePull = bannerStats.getRarityProbability();
+        double rarityPityProbability = bannerStats.getRarityPityProbability();
+        int rarityPityCount = bannerStats.getPityCount();
+
+        double finalProbability = 0.0;
+
+        List<Integer> pullsList = pullSim.userPullsSim(singlePull, rarityPityProbability, rarityPityCount);
+        double averagePulls = pullSim.averagePullsForDesiredRarity(pullsList);
+        double pullSimProbability = pullSim.probabilityForDesiredRarity(pullsList);
+
 
         if (gameTitle.equals("Genshin Impact")){
             boolean isGuaranteed = input.genshinQuestion(gameTitle);
@@ -47,21 +59,29 @@ public class Main {
             else {
                 finalProbability = calculate.genshinProbability(singlePull,numOfPulls);
             }
+        } else if (gameTitle.equals("Pokemon TCGP")){
+            finalProbability = calculate.complementaryProbability(singlePull, numOfPulls);
+        } else if (gameTitle.equals("Infinity Nikki")) {
+            finalProbability = pullSimProbability;
         }
 
 
+
+        /*
         String formatted = formatOutput.roundedProbability(finalProbability);
         output.print(formatted);
+         */
 
-        System.out.println();
-
-        how.howItsDone(gameTitle);
 
         System.out.println();
         System.out.println("Game: " + gameTitle);
         System.out.println("Banner/Pack: " + bannerName);
         System.out.println("Rarity: " + rarity);
         System.out.println("Final Probability: " + finalProbability);
+        System.out.println("Average Number of Pulls Based on 10,000 Simulated Successes: ~" + averagePulls);
+        System.out.println("Probability based on 10,000 Simulated Successes: ~" + pullSimProbability + "\n");
+
+        how.howItsDone(gameTitle);
 
 
 
