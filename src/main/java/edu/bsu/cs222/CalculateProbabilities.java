512f52b096308f9eb788a2a6feba352928341c0e
Casity <cassandra.lewis4503@gmail.com>
2025-11-12 20:41:13 -0500

    added methods for genshin no test cases yet

--- src/main/java/edu/bsu/cs222/CalculateProbabilities.java
+++ src/main/java/edu/bsu/cs222/CalculateProbabilities.java
@@ -8,25 +8,36 @@ public class CalculateProbabilities {
         return Math.pow(1 - (1 - probability), numOfPulls);
     }
 
+   /* public double infinityNikiProbability(double probability, double pity, int numOfPulls){
+
+
+
+        return finalProbability;
+   */
+
     public double genshinProbability(double softProbability, int numOfPulls) {
-        double finalProbability = 0;
-
-        double hardPity = .06;
-
-        for (int i = 0; i <= numOfPulls; i++) {
-
-            if (i == 90) {
-                finalProbability = 1;
-            } else if (i > 73) {
-
-                finalProbability = softProbability + (hardPity * (i - 73));
-
-            } else finalProbability = softProbability;
 
 
+
+            double finalProbability = 0;
+
+            double hardPity = .06;
+
+            for (int i = 0; i <= numOfPulls; i++) {
+
+                if (i == 90) {
+                    finalProbability = 1;
+                } else if (i > 73) {
+
+                    finalProbability = softProbability + (hardPity * (i - 73));
+
+                } else finalProbability = softProbability;
+
+
+            }
+            return finalProbability * 100;
         }
-        return finalProbability * 100;
-    }
+
 
 
     public double genshinProbability5050(double softProbability, int numOfPulls) {
@@ -46,9 +57,49 @@ public class CalculateProbabilities {
 
 
         }
-        return (finalProbability * 100)*.5;
+        return (finalProbability * 100) * .5;
+    }
+
+    public double genshinProbablity4StarCharacter(double softProbability, int numOfPulls){
+
+        double finalProbability = 0;
+        int numOfCharacters = 0;
+        if( numOfPulls %10 == 0){
+           finalProbability = .5;
+            if(numOfPulls %20 == 0){
+                int numberOfCharacters = numOfPulls/20;
+                finalProbability = 1;
+            }
+        } else{
+            finalProbability= Math.pow(1 - (1 - softProbability), numOfPulls);
+        }
+
+
+
+
+return finalProbability;
+    }
+
+    public double genshinProbablity4Wepon(double softProbability, int numOfPulls){
+
+        double finalProbability = 0;
+        int numOfCharacters = 0;
+        if( numOfPulls %10 == 0){
+            finalProbability = .1;
+
+        } else{
+            finalProbability= Math.pow(1 - (1 - softProbability), numOfPulls);
+        }
+
+
+
+
+        return finalProbability;
     }
 }
+
+
+
 
 
 
