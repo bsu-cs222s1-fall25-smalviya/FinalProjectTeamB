5902dbfed9f1b4b2c1f57d2e42ffc78805842eba
tatumph <149801019+tatumph@users.noreply.github.com>
2025-11-18 18:28:16 -0500

    Got started on UI work

---
+++ src/main/java/edu/bsu/cs222/ApplicationController.java
@@ -0,0 +1,9 @@
+package edu.bsu.cs222;
+
+import javafx.event.ActionEvent;
+
+public class ApplicationController {
+    public void exitProgram(ActionEvent actionEvent) {
+        System.exit(0);
+    }
+}
