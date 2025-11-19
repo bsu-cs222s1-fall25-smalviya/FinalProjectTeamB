3ce3627ec3880fcf73584084b6705a1eafd56b8b
tatumph <149801019+tatumph@users.noreply.github.com>
2025-11-18 19:10:15 -0500

    Did some more UI work.Looking into making a graph for showing the simulation results. Will make a test UI to be safe

--- src/main/java/edu/bsu/cs222/ApplicationController.java
+++ src/main/java/edu/bsu/cs222/ApplicationController.java
@@ -1,8 +1,23 @@
 package edu.bsu.cs222;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 
 public class ApplicationController {
+    @FXML
+    private ChoiceBox<String> GachaGameChoice;
+
+    public void initializeApplication(){
+        GachaGameChoice.setItems(FXCollections.observableArrayList("Pokemon TCGP","Infinity Nikki","Genshin Impact"));
+    }
+
+    @FXML
+    String getGachaGameChoice(ActionEvent event) {
+        return (String) GachaGameChoice.getValue();
+    }
+
     public void exitProgram(ActionEvent actionEvent) {
         System.exit(0);
     }
