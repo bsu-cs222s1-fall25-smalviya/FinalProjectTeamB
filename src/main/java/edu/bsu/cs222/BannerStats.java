30900a4d7bd4ab49ebb1cebff4e3751442e84f24
tatumph <149801019+tatumph@users.noreply.github.com>
2025-12-03 15:51:41 -0500

    Changed back to class from record

--- src/main/java/edu/bsu/cs222/BannerStats.java
+++ src/main/java/edu/bsu/cs222/BannerStats.java
@@ -1,4 +1,24 @@
 package edu.bsu.cs222;
 
-public record BannerStats(String rarity, double rarityProbability, double rarityPityProbability, int pityCount) {
-}
+public class BannerStats {
+    private final String rarity;
+    private final double rarityProbability;
+    private final double rarityPityProbability;
+    private final int pityCount;
+
+    public BannerStats(String rarity, double rarityProbability, double rarityPityProbability, int pityCount){
+        this.rarity = rarity;
+        this.rarityProbability = rarityProbability;
+        this.rarityPityProbability = rarityPityProbability;
+        this.pityCount = pityCount;
+    }
+
+    public String getRarity() {return rarity;}
+
+    public double getRarityProbability() {return rarityProbability;}
+
+    public double getRarityPityProbability() {return rarityPityProbability;}
+
+    public int getPityCount() {return pityCount;}
+
+    }
